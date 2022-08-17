b3f754ee9555699ad377226d1770aa34d866e581
Your Name <you@example.com>
2022-08-17 22:37:46 +0900

    5-3

---
+++ 4-01_input/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+document.getElementById('form').onsubmit = function(event){
+  // console.log('クリックされました');
+  event.preventDefault();
+  const search = document.getElementById('form').word.value;
+  document.getElementById('output').textContent = `「${search}」の検索中...`;
+};
